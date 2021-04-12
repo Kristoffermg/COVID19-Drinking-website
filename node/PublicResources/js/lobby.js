c018d1f5e1ed176e678a769f9d59921fad1f3a0c
Koidesoikz <73647151+Koidesoikz@users.noreply.github.com>
2021-04-12 14:31:23 +0200

    hejsan

--- node/PublicResources/js/lobby.js
+++ node/PublicResources/js/lobby.js
@@ -1,4 +1,5 @@
 const socket = io({path:'/node0/socket.io', transports: ["polling"], autoConnect: false});  
+console.log("socket connected" + socket.connected);
 if (!socket.connected) {
     console.log("Entered weirdass if statement");
     socket.connect;
