1bee278da97340897d47cdf2b1464829c0a56fe7
Koidesoikz <73647151+Koidesoikz@users.noreply.github.com>
2021-04-13 12:35:07 +0200

    Update quickMeme.js

--- node/PublicResources/js/quickMeme.js
+++ node/PublicResources/js/quickMeme.js
@@ -1,6 +1,6 @@
 console.log("oh no");
 
-debugMeme = document.getElementById("debugMeme2");
+debugMeme = document.getElementById("debugMeme");
 
 debugMeme.addEventListener("click", () => {
     socket.emit('startGame', 'test2');
