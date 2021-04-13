f2181ec03215e9ca5203bd7d96ffce2aa5c6dcf7
Koidesoikz <73647151+Koidesoikz@users.noreply.github.com>
2021-04-13 12:16:03 +0200

    måske det virker nu

--- node/PublicResources/js/lobby.js
+++ node/PublicResources/js/lobby.js
@@ -112,31 +112,6 @@ copyUrl.addEventListener("click", () => {
     document.execCommand("copy");
 });
 
-socket.on('changeHTML', meme => {
-    let body = document.getElementById("body");
-    let head = document.getElementById("head");
-    newMeme = String(meme);
-    let splitMeme = newMeme.split('<body id="body">')[1];
-    splitMeme = splitMeme.split("</body>")[0];
-    
-    let videos = document.createElement("div");
-    videos = document.getElementById("videos");
-    body.remove();
-    let newBody = document.createElement("body");
-    newBody.innerHTML = splitMeme;
-    head.after(newBody);
-    let videoPlacement = document.getElementById("usernameInput");
-    videoPlacement.after(videos);
-
-    let scriptPlaceholder = document.getElementById("pageScript");
-    let src = scriptPlaceholder.getAttribute("src");
-    
-    let pageScript = document.createElement("script");
-    pageScript.src = src;
-    scriptPlaceholder.remove();
-    document.body.appendChild(pageScript);
-});
-
 socket.on('changeName', name =>{
     console.log("Username: " + socket.userName);
 });
--- node/PublicResources/js/socketConnection.js
+++ node/PublicResources/js/socketConnection.js
@@ -4,3 +4,28 @@ if (!socket.connected) {
     console.log("Entered weirdass if statement");
     socket.connect();
 }
+
+socket.on('changeHTML', meme => {
+    let body = document.getElementById("body");
+    let head = document.getElementById("head");
+    newMeme = String(meme);
+    let splitMeme = newMeme.split('<body id="body">')[1];
+    splitMeme = splitMeme.split("</body>")[0];
+    
+    let videos = document.createElement("div");
+    videos = document.getElementById("videos");
+    body.remove();
+    let newBody = document.createElement("body");
+    newBody.innerHTML = splitMeme;
+    head.after(newBody);
+    let videoPlacement = document.getElementById("usernameInput");
+    videoPlacement.after(videos);
+
+    let scriptPlaceholder = document.getElementById("pageScript");
+    let src = scriptPlaceholder.getAttribute("src");
+    
+    let pageScript = document.createElement("script");
+    pageScript.src = src;
+    scriptPlaceholder.remove();
+    document.body.appendChild(pageScript);
+});
