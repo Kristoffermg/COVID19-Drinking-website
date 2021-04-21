c990ea9fc86c5d246f26c4ea175da5af1f6e0708
MartinSvanholm <msvanh20@student.aau.dk>
2021-04-21 15:18:46 +0200

    - games

--- node/PublicResources/js/never.js
+++ node/PublicResources/js/never.js
@@ -1,16 +1,18 @@
 let answerYes = 0;  //Counter which shows how many rounds one has answered yes
 let answerNo = 0;   //Counter which shows how many rounds one has answered no
 
-let timerBar = document.getElementById("timer")
+let roundtimeBar = document.querySelector(".round-time-bar")
 let neverText = document.getElementById("neverText")
 let nextText = document.getElementById("nextText")
 let iHave = document.getElementById("iHave");
 let iHaveNever = document.getElementById("iHaveNever");
+let sipText = document.getElementById("sipText");
 
 
 /* change the timer bar seconds whatever 
 document.getElementById("timer").style.setProperty("--duration", 10)
 */ 
+
 
 neverText.innerText = "Click \"Next round\" to start the game"
 
@@ -25,6 +27,13 @@ nextText.addEventListener("click", () => {
 
     iHaveNever.style.opacity = 0.9;
     iHaveNever.disabled = false;
+
+    sipText.style.display = "none";
+
+    roundtimeBar.style.display = "block";
+    roundtimeBar.classList.remove("round-time-bar");
+    roundtimeBar.offsetWidth;
+    roundtimeBar.classList.add("round-time-bar");
 });
 
 socket.on('activateNextRoundBtn', () => {
@@ -48,8 +57,15 @@ socket.on('gameOver', () => {
 });
 
 socket.on('countdownTick', () => {
+    /*
     console.log("yooooooooooooooooooooooooooooooooooooooooooo im in countdownTick!")
-    let duration = getComputedStyle(document.documentElement)
+    let duration = getComputedStyle(timerBar)
         .getPropertyValue('--duration')
+    console.log("duration: " + duration)
     timerBar.style.setProperty('--duration', 10)
+    */
+})
+
+iHave.addEventListener("click", () => {
+    sipText.style.display = "block";
 })
