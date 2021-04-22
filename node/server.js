c648dd2dde0418a5d7509d69f11539b25bb29ce3
Koidesoikz <73647151+Koidesoikz@users.noreply.github.com>
2021-04-22 15:22:29 +0200

    Update server.js

--- node/server.js
+++ node/server.js
@@ -30,11 +30,11 @@ console.log(path);
 app.use(express.static(path));
 
 app.get('/', function(req, res) {
-    res.sendFile(pathApi.join(__dirname + '/PublicResources/htmlLocal/index.html'));
+    res.sendFile(pathApi.join(__dirname + '/PublicResources/html/index.html'));
 });
 
 app.get('/Lobby', function(req, res) {
-    fs.readFile(__dirname + '/PublicResources/htmlLocal/createlobby.html', 'utf8', function(err, data) {
+    fs.readFile(__dirname + '/PublicResources/html/createlobby.html', 'utf8', function(err, data) {
         if (err) throw err;
         //console.log(data);
         res.send(data);
@@ -50,7 +50,7 @@ app.get('/Lobby/:lobbyId', function(req, res) {
 
     for (let i = 0; i < idArr.length; i++) {
         if (idArr[i].roomId == lobbyId) {
-            fs.readFile(__dirname + '/PublicResources/htmlLocal/createlobby.html', 'utf8', function(err, data) {
+            fs.readFile(__dirname + '/PublicResources/html/createlobby.html', 'utf8', function(err, data) {
                 if (err) throw err;
                 //console.log(data);
                 res.send(data);
@@ -62,7 +62,7 @@ app.get('/Lobby/:lobbyId', function(req, res) {
 });
 
 app.get('/GamesAndRules', function(req, res) {
-    fs.readFile(__dirname + '/PublicResources/htmlLocal/gamesAndRules.html', 'utf8', function(err, data) {
+    fs.readFile(__dirname + '/PublicResources/html/gamesAndRules.html', 'utf8', function(err, data) {
         if (err) throw err;
         //console.log(data);
         res.send(data);
@@ -131,7 +131,7 @@ io.on('connection', (socket) => {
 
     //haha debug go brr
     socket.on('debugMeme', () => {
-        fs.readFile(__dirname + '/PublicResources/htmlLocal/createlobbyMeme.html', 'utf8', function(err, data) {
+        fs.readFile(__dirname + '/PublicResources/html/createlobbyMeme.html', 'utf8', function(err, data) {
             if (err) throw err;
             io.to(socket.room).emit('debugMeme', data);
         });
@@ -190,7 +190,7 @@ io.on('connection', (socket) => {
                 case 'prompt':
                     console.log("Prompt game chosen");
                     //Throw prompt html
-                    htmlPath = '/PublicResources/htmlLocal/never.html';
+                    htmlPath = '/PublicResources/html/never.html';
                     //Initialize 'Never have I ever' variables
                     for (let i = 0; i < idArr.length; i++) {
                         if (idArr[i].roomId == socket.room) {
@@ -209,21 +209,21 @@ io.on('connection', (socket) => {
                 case 'card':
                     console.log("Card game chosen");
                     //Throw card html
-                    htmlPath = '/PublicResources/htmlLocal/createlobby.html'; //<-- Midlertidig path så ting ikk explodere
+                    htmlPath = '/PublicResources/html/createlobby.html'; //<-- Midlertidig path så ting ikk explodere
                     break;
     
                 case 'dice':
                     console.log("Dice game chosen");
                     //Throw dice html
-                    htmlPath = '/PublicResources/htmlLocal/createlobby.html'; //<-- Midlertidig path så ting ikk explodere
+                    htmlPath = '/PublicResources/html/createlobby.html'; //<-- Midlertidig path så ting ikk explodere
                     break;
                 
                 case 'test1':
-                    htmlPath = '/PublicResources/htmlLocal/createlobbyMeme.html';
+                    htmlPath = '/PublicResources/html/createlobbyMeme.html';
                     break;
                 
                 case 'test2':
-                    htmlPath = '/PublicResources/htmlLocal/createlobby.html';
+                    htmlPath = '/PublicResources/html/createlobby.html';
                     break;
     
                 default:
