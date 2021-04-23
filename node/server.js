a8851962bb42348b546c7ece3330619c0ebc8fff
Koidesoikz <73647151+Koidesoikz@users.noreply.github.com>
2021-04-23 13:08:27 +0200

    asdasd

--- node/server.js
+++ node/server.js
@@ -30,11 +30,11 @@ console.log(path);
 app.use(express.static(path));
 
 app.get('/', function(req, res) {
-    res.sendFile(pathApi.join(__dirname + '/PublicResources/html/index.html'));
+    res.sendFile(pathApi.join(__dirname + '/PublicResources/htmlLocal/index.html'));
 });
 
 app.get('/Lobby', function(req, res) {
-    fs.readFile(__dirname + '/PublicResources/html/createlobby.html', 'utf8', function(err, data) {
+    fs.readFile(__dirname + '/PublicResources/htmlLocal/createlobby.html', 'utf8', function(err, data) {
         if (err) throw err;
         //console.log(data);
         res.send(data);
@@ -45,24 +45,26 @@ app.get('/Lobby/:lobbyId', function(req, res) {
     let lobbyId = req.params.lobbyId;
     console.log(lobbyId);
     if (idArr.length <= 0) {
-        res.redirect('/node0/');      //Changed from /node0/
+        // res.redirect('/');      //Changed from /node0/
+        res.sendFile(pathApi.join(__dirname + '/PublicResources/htmlLocal/error.html'));
     }
 
     for (let i = 0; i < idArr.length; i++) {
         if (idArr[i].roomId == lobbyId) {
-            fs.readFile(__dirname + '/PublicResources/html/createlobby.html', 'utf8', function(err, data) {
+            fs.readFile(__dirname + '/PublicResources/htmlLocal/createlobby.html', 'utf8', function(err, data) {
                 if (err) throw err;
                 //console.log(data);
                 res.send(data);
             });
         } else {
-            res.redirect('/node0/');  //Changed from /node0/
+            // res.redirect('/');  //Changed from /node0/
+            res.sendFile(pathApi.join(__dirname + '/PublicResources/htmlLocal/error.html'));
         }
     }
 });
 
 app.get('/GamesAndRules', function(req, res) {
-    fs.readFile(__dirname + '/PublicResources/html/gamesAndRules.html', 'utf8', function(err, data) {
+    fs.readFile(__dirname + '/PublicResources/htmlLocal/gamesAndRules.html', 'utf8', function(err, data) {
         if (err) throw err;
         //console.log(data);
         res.send(data);
@@ -131,7 +133,7 @@ io.on('connection', (socket) => {
 
     //haha debug go brr
     socket.on('debugMeme', () => {
-        fs.readFile(__dirname + '/PublicResources/html/createlobbyMeme.html', 'utf8', function(err, data) {
+        fs.readFile(__dirname + '/PublicResources/htmlLocal/createlobbyMeme.html', 'utf8', function(err, data) {
             if (err) throw err;
             io.to(socket.room).emit('debugMeme', data);
         });
@@ -190,7 +192,7 @@ io.on('connection', (socket) => {
                 case 'prompt':
                     console.log("Prompt game chosen");
                     //Throw prompt html
-                    htmlPath = '/PublicResources/html/never.html';
+                    htmlPath = '/PublicResources/htmlLocal/never.html';
                     //Initialize 'Never have I ever' variables
                     for (let i = 0; i < idArr.length; i++) {
                         if (idArr[i].roomId == socket.room) {
@@ -209,21 +211,21 @@ io.on('connection', (socket) => {
                 case 'card':
                     console.log("Card game chosen");
                     //Throw card html
-                    htmlPath = '/PublicResources/html/createlobby.html'; //<-- Midlertidig path så ting ikk explodere
+                    htmlPath = '/PublicResources/htmlLocal/createlobby.html'; //<-- Midlertidig path så ting ikk explodere
                     break;
     
                 case 'dice':
                     console.log("Dice game chosen");
                     //Throw dice html
-                    htmlPath = '/PublicResources/html/createlobby.html'; //<-- Midlertidig path så ting ikk explodere
+                    htmlPath = '/PublicResources/htmlLocal/createlobby.html'; //<-- Midlertidig path så ting ikk explodere
                     break;
                 
                 case 'test1':
-                    htmlPath = '/PublicResources/html/createlobbyMeme.html';
+                    htmlPath = '/PublicResources/htmlLocal/createlobbyMeme.html';
                     break;
                 
                 case 'test2':
-                    htmlPath = '/PublicResources/html/createlobby.html';
+                    htmlPath = '/PublicResources/htmlLocal/createlobby.html';
                     break;
     
                 default:
